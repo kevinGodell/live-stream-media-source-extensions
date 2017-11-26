0eab88c093661362d6dbddc4e9fb1fb889421a0e
Kevin Godell <kevin.godell@gmail.com>
2017-11-26 00:43:50 -0600

    added tests for flv.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const Mp4Segmenter = new require('./Mp4Segmenter');
 
 const mp4segmenter = new Mp4Segmenter();
 
-const ffmpeg = spawn('ffmpeg', ['-loglevel', 'debug', '-reorder_queue_size', '5', '-rtsp_transport', 'tcp', '-i', 'rtsp://131.95.3.162/axis-media/media.3gp', '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'], {stdio : ['ignore', 'pipe', 'inherit'/* change stdio[2] inherit to ignore to hide ffmpeg debug to stderr */]});
+const ffmpeg = spawn('ffmpeg', ['-loglevel', 'debug', '-reorder_queue_size', '5', '-rtsp_transport', 'tcp', '-i', 'rtsp://192.168.1.22:554/user=admin_password=pass_channel=1_stream=0.sdp', '-an', '-c:v', 'copy', '-f', 'mp4', '-movflags', '+frag_keyframe+empty_moov+default_base_moof', '-metadata', 'title="media source extensions"', 'pipe:1'], {stdio : ['ignore', 'pipe', 'inherit'/* change stdio[2] inherit to ignore to hide ffmpeg debug to stderr */]});
 
 ffmpeg.on('error', (error) => {
     console.log('error', error);
@@ -29,6 +29,42 @@ ffmpeg.stdio[1].pipe(mp4segmenter);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/flv.min.js', (req, res) => {
+    res.sendFile(__dirname + '/node_modules/flv.js/dist/flv.min.js');
+});
+
+app.get('/test.mp4', (req, res) => {
+    if (!mp4segmenter.initSegment) {
+        res.status(503);
+        res.end('service not available');
+        return;
+    }
+    
+    res.status(200);
+    res.write(mp4segmenter.initSegment);
+    ffmpeg.stdio[1].pipe(res);
+    res.on('close', () => {
+        ffmpeg.stdio[1].unpipe(res);
+    });
+});
+
+app.get('/test2.mp4', (req, res) => {
+    if (!mp4segmenter.initSegment) {
+        res.status(503);
+        res.end('service not available');
+        return;
+    }
+    function writeSegment(data) {
+        res.write(data);
+    }
+    res.status(200);
+    res.write(mp4segmenter.initSegment);
+    mp4segmenter.on('segment', writeSegment);
+    res.on('close', () => {
+        mp4segmenter.removeListener('segment', writeSegment);
+    });
 });
 
 io.on('connection', (socket) => {
